20e83deae7f463230fd8c7940b809aa0c61a93f0
JoseCostaUni <up202207871@up.pt>
2024-02-28 16:02:02 +0000

    Testing graph

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include "Logic/stdafx.h"
 
+enum class teste{
+    triangulo,
+    circulo
+};
 
 int main() {
 
@@ -7,7 +11,7 @@ int main() {
     std::vector<PumpingStations> pumpingStations;
     std::vector<WaterReservoir> waterReservations;
 
-
+    Graph<teste> g;
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
